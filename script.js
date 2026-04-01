7b892a52cd5df2ce205ab13290a8d024a7c7f600
Lily Spiniolas <lily_spiniolas@apple.com>
2026-04-01 01:09:09 -0700

    Add auxclick example page

---
+++ script.js
@@ -0,0 +1,27 @@
+function logMouseEvent(e, element) {
+    element.innerText = "MouseEvent.altKey: " + e.altKey + "\n" +
+        "MouseEvent.button: " + e.button + "\n" +
+        "MouseEvent.buttons: " + e.buttons + "\n" +
+        "MouseEvent.clientCoords: (" + e.clientX + ", " + e.clientY + ")" + "\n" +
+        "MouseEvent.ctrlKey: " + e.ctrlKey + "\n" +
+        "MouseEvent.layerCoords: (" + e.layerX + ", " + e.layerY + ")" + "\n" +
+        "MouseEvent.metaKey: " + e.metaKey + "\n" +
+        "MouseEvent.movementCoords: (" + e.movementX + ", " + e.movementY + ")" + "\n" +
+        "MouseEvent.offsetCoords: (" + e.offsetX + ", " + e.offsetY + ")" + "\n" +
+        "MouseEvent.pageCoords: (" + e.pageX + ", " + e.pageY + ")" + "\n" +
+        "MouseEvent.relatedTarget: " + e.relatedTarget + "\n" +
+        "MouseEvent.screenCoords: (" + e.pageX + ", " + e.pageY + ")" + "\n" +
+        "MouseEvent.shiftKey: " + e.shiftKey + "\n" +
+        "MouseEvent.webkitForce: " + e.webkitForce + "\n" +
+        "MouseEvent.x: " + e.x + "\n" +
+        "MouseEvent.y: " + e.y + "\n"
+}
+
+function formatMouseEventLog(element) {
+    element.innerHTML = element.innerText.split("\n").map((line, j) => {
+        let i = line.indexOf(":");
+        if (i === -1) return line;
+        let cls = j % 2 === 0 ? "property-value" : "property-value-alt";
+        return "<span class='" + cls + "'><span class='property'>" + line.slice(0, i + 1) + "</span> <span class='value'>" + line.slice(i + 2) + "</span></span>";
+    }).join("<br>")
+}
